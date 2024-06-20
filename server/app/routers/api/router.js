c9ed38bb0b11fec75bab3e6c7120cb4c17812b56
iremchabanne <irem_kcakir@outlook.com>
2024-06-20 19:15:51 +0200

    Setting up a database

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,25 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-const { sayWelcome } = require("../../controllers/sayActions");
-
-router.get("/", sayWelcome);
-
-/* ************************************************************************* */
-// Import And Use Routers Here
-/* ************************************************************************* */
 const categoriesRouter = require("./categories/router");
 
 router.use("/categories", categoriesRouter);
-
-const programsRouter = require("./programs/router");
-
-router.use("/programs", programsRouter);
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
 
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 /* ************************************************************************* */
 
 module.exports = router;
